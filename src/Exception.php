93ee1242dfbdc908523f7f07d134fbcebf2350d6
Andrew Roslik <andkirby@gmail.com>
2016-09-22 15:57:34 +0300

    Updated phpdoc.

--- src/Exception.php
+++ src/Exception.php
@@ -1,13 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: a.roslik
- * Date: 9/22/16 022
- * Time: 1:18 PM
- */
-
 namespace Rikby\SymfonyConsole\CommandCollector;
 
+/**
+ * Local Exception
+ *
+ * @package Rikby\SymfonyConsole\CommandCollector
+ */
 class Exception extends \Exception
 {
 
